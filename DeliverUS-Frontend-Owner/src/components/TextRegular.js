0b869e3f9434f3fe6ad3830b30c1aa5086d52669
Javierchapi <javchagar@alum.us.es>
2026-03-11 18:36:50 +0100

    explicacion textStyle

--- DeliverUS-Frontend-Owner/src/components/TextRegular.js
+++ DeliverUS-Frontend-Owner/src/components/TextRegular.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { StyleSheet, Text } from 'react-native'
 export default function TextRegular(props) {
   const { textStyle, ...inputProps } = props
-
+  //TextStyle cogeria el valor de la propiedad TextStyle de props, y el resto de propiedades de props van a inputProps
   return (
     <Text style={[styles.text, textStyle]} {...inputProps}>
       {props.children}
